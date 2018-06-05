45070315524093dfe8aba8de2f2253387741c49b
Katie_Norm <kuhl1@ford.com>
2018-06-05 14:45:13 -0600

    Added time_entries2 table

--- databases/tracker/migrations/V2__initial_schema.sql
+++ databases/tracker/migrations/V2__initial_schema.sql
@@ -1,9 +1,10 @@
-CREATE TABLE time_entries (
+CREATE TABLE time_entries2 (
   id         BIGINT(20) NOT NULL AUTO_INCREMENT,
   project_id BIGINT(20),
   user_id    BIGINT(20),
   date       DATE,
   hours      INT,
+  new        VARCHAR(10),
   PRIMARY KEY (id)
 )
   ENGINE = innodb
